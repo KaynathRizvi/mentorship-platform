555e5fff8d1046aff37d4e7f8a81e5f713f2a302
Kaynath Zehra Rizvi <rizvikaynath@gmail.com>
2026-04-03 18:05:46 +0530

    6

--- server/routes/mentorRoutes.js
+++ server/routes/mentorRoutes.js
@@ -10,7 +10,7 @@ router.post("/login", login);
 
 router.get("/featured", async (req, res) => {
   try {
-    const mentors = await Mentor.find({ isFeatured: true }).limit(10);
+    const mentors = await Mentor.find().sort({ isFeatured: -1 })
     res.json(mentors);
   } catch (err) {
     res.status(500).json({ message: err.message });
